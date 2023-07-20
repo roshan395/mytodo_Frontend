5840faa8eadfd00bacef84724cda05b3d6177914
unknown <roushanraj395@gmail.com>
2023-07-20 12:52:28 +0530

    passing params between components

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import {BrowserRouter, Route, Routes, useNavigate} from 'react-router-dom'
+import {BrowserRouter, Route, Routes, useNavigate, useParams} from 'react-router-dom'
 import './TodoApp.css'
 
 export default function TodoApp() {
@@ -9,7 +9,7 @@ export default function TodoApp() {
                 <Routes>
                 <Route path='/' element={<LoginComponent/>}></Route>
                     <Route path='/login' element={<LoginComponent/>}></Route>
-                    <Route path='/welcome' element={<WelcomeComponent/>}></Route>
+                    <Route path='/welcome/:username' element={<WelcomeComponent/>}></Route>
                     <Route path='*' element={<ErrorComponent/>}></Route>
                 </Routes>
             </BrowserRouter>
@@ -40,7 +40,7 @@ function LoginComponent() {
         if(username==='roshan' && password === 'dummy'){
             setShowSuccessMessage(true)
             setshowErrorMessage(false)
-            navigate('/welcome')
+            navigate(`/welcome/${username}`)
         } else {
             setShowSuccessMessage(false)
             setshowErrorMessage(true)
@@ -70,9 +70,11 @@ function LoginComponent() {
 }
 
 function WelcomeComponent() {
+    const {username} = useParams()
+    console.log(username)
     return(
         <div className="WelcomeComponent">
-            <h1>Welcome</h1>
+            <h1>Welcome {username}</h1>
             Welcome Component
         </div>
     )
