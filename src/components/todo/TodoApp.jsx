0efcccb5ac16ba53109c4cf84ab2873e8cfe015b
unknown <roushanraj395@gmail.com>
2023-07-20 17:32:09 +0530

    added todos

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import {BrowserRouter, Route, Routes, useNavigate, useParams} from 'react-router-dom'
+import {BrowserRouter, Route, Routes, useNavigate, useParams, Link} from 'react-router-dom'
 import './TodoApp.css'
 
 export default function TodoApp() {
@@ -7,10 +7,11 @@ export default function TodoApp() {
         <div className="TodoApp">
             <BrowserRouter>
                 <Routes>
-                <Route path='/' element={<LoginComponent/>}></Route>
-                    <Route path='/login' element={<LoginComponent/>}></Route>
-                    <Route path='/welcome/:username' element={<WelcomeComponent/>}></Route>
-                    <Route path='*' element={<ErrorComponent/>}></Route>
+                <Route path='/' element={<LoginComponent/>}/>
+                    <Route path='/login' element={<LoginComponent/>}/>
+                    <Route path='/welcome/:username' element={<WelcomeComponent/>}/>
+                    <Route path='/todos' element={<ListTodosComponent/>}/>
+                    <Route path='*' element={<ErrorComponent/>}/>
                 </Routes>
             </BrowserRouter>
         </div>
@@ -75,7 +76,9 @@ function WelcomeComponent() {
     return(
         <div className="WelcomeComponent">
             <h1>Welcome {username}</h1>
-            Welcome Component
+            <div>
+                Manage your Todos - <Link to="/todos">here</Link>
+            </div>
         </div>
     )
 }
@@ -86,6 +89,50 @@ function ErrorComponent() {
             <h1>We are working on it</h1>
             <div>
                 apologies for 404, rech out to tem at ABC-DEF-GHI.
+            </div>
+        </div>
+    )
+}
+
+function ListTodosComponent() {
+
+    const today = new Date()
+    const targetDate = new Date(today.getFullYear()+1, today.getMonth(), today.getDay())
+    const todos = [
+        {id:1, description:"learn AWS", done:false, targetDate:targetDate},
+        {id:2, description:"learn AZURE", done:false, targetDate:targetDate},
+        {id:3, description:"learn Devops", done:false, targetDate:targetDate}
+    ]
+
+    return(
+        <div className="ListTodosComponent">
+            <h1>Things you want to do!</h1>
+            <div>
+                <table>
+                    <thead>
+                        <tr>
+                            <td>Id</td>
+                            <td>Description</td>
+                            <td>Done</td>
+                            <td>Target Date</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            todos.map(
+                                todo => (
+                                    <tr key={todo.id}>
+                                        <td>{todo.id}</td>
+                                        <td>{todo.description}</td>
+                                        <td>{todo.done.toString()}</td>
+                                        <td>{todo.targetDate.toDateString()}</td>
+                                    </tr>
+                                )
+                            )
+                        }
+                        
+                    </tbody>
+                </table>
             </div>
         </div>
     )
