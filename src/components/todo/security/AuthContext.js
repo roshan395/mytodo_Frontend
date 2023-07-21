71f79ea0e95432bc1b32b5e2a87ee0c5381e20f2
unknown <roushanraj395@gmail.com>
2023-07-21 14:01:37 +0530

    created hook to easily access the context

--- src/components/todo/security/AuthContext.js
+++ src/components/todo/security/AuthContext.js
@@ -1,13 +1,18 @@
-import { createContext, useState } from "react";
+import { createContext, useContext, useState } from "react";
 
 //1.create context
 export const AuthContext = createContext()
+
+export const useAuth = () => useContext(AuthContext)
 
 //2.share the context with other components
 export default function AuthProvider({children}) {
 
     //3.put some state in context
     const [number, setNumber] = useState(10)
+
+    setInterval( () => setNumber(number+1), 10000 )
+
     return (
         <AuthContext.Provider value={ {number} }>
             {children}
