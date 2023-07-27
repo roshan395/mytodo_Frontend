ecde8844ae775639bc5c4f05d26e1d7a3c3110aa
unknown <roushanraj395@gmail.com>
2023-07-27 12:29:03 +0530

    added validations

--- src/components/todo/TodoComponent.jsx
+++ src/components/todo/TodoComponent.jsx
@@ -2,12 +2,14 @@ import { useParams } from "react-router-dom"
 import { retrieveTodoApi } from "./api/TodoApiService"
 import { useAuth } from "./security/AuthContext"
 import { useEffect, useState } from "react"
+import { ErrorMessage, Field, Form, Formik } from "formik"
 
 export default function TodoComponent() {
     const {id} = useParams()
     const authContext = useAuth()
     const username = authContext.username
     const [description, setDescription] = useState('')
+    const [targetDate, settargetDate] = useState('')
 
     useEffect(() => retrieveTodo(), [id])
 
@@ -16,15 +18,63 @@ export default function TodoComponent() {
         .then(response => {
             console.log(response.data)
             setDescription(response.data.description)
+            settargetDate(response.data.targetDate)
         })
         .catch(error => console.log(error))    
+    }
+
+    function onSubmit(values) {
+        console.log(values)
+    }
+
+    function validate(values) {
+        let errors = {
+            // description: 'enter a valid desception'
+        }
+        if(values.description.length < 5){
+            errors.description = 'Eneter atleast 5 characters'
+        }
+        if(values.targetDate == null) {
+            errors.targetDate = 'Enter target date'
+        }
+        console.log(values)
+        return errors
     }
 
     return(
         <div className="container">
             <h1>Enter Todo Details</h1>
             <div>
-                description: {description}
+                <Formik initialValues={{description, targetDate}}
+                    enableReinitialize = {true}
+                    onSubmit={onSubmit}
+                    validate={validate}
+                    validateOnChange={false}
+                    validateOnBlur={false}
+                >
+                {
+                    (props) => (
+                        <Form>
+                            <ErrorMessage
+                                name="description"
+                                component="div"
+                                className="alert alert-warning"
+                            />
+                            <fieldset className="form-group">
+                                <label>Description</label>
+                                <Field type="text" className="form-control" name="description"></Field>
+                            </fieldset>
+                            <fieldset className="form-group">
+                                <label>Target Date</label>
+                                <Field type="date" className="form-control" name="targetDate"></Field>
+                            </fieldset>
+                            <div>
+                                <button className="btn btn-success m-5" type="submit">Save</button>
+                            </div>
+                        </Form>
+                    )
+                }
+                </Formik>
             </div>
         </div>
     )
