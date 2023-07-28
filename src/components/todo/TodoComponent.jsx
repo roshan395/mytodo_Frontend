d4ed969daa21002ed8f83cf1143b6e1fa6228451
unknown <roushanraj395@gmail.com>
2023-07-28 12:54:12 +0530

    added updateapi functionality

--- src/components/todo/TodoComponent.jsx
+++ src/components/todo/TodoComponent.jsx
@@ -1,5 +1,5 @@
-import { useParams } from "react-router-dom"
-import { retrieveTodoApi } from "./api/TodoApiService"
+import { useNavigate, useParams } from "react-router-dom"
+import { retrieveTodoApi, updateTodoApi } from "./api/TodoApiService"
 import { useAuth } from "./security/AuthContext"
 import { useEffect, useState } from "react"
 import { ErrorMessage, Field, Form, Formik } from "formik"
@@ -7,6 +7,7 @@ import { ErrorMessage, Field, Form, Formik } from "formik"
 export default function TodoComponent() {
     const {id} = useParams()
     const authContext = useAuth()
+    const navigate = useNavigate()
     const username = authContext.username
     const [description, setDescription] = useState('')
     const [targetDate, settargetDate] = useState('')
@@ -20,11 +21,25 @@ export default function TodoComponent() {
             setDescription(response.data.description)
             settargetDate(response.data.targetDate)
         })
-        .catch(error => console.log(error))    
+        .catch(error => console.log(error))
     }
 
     function onSubmit(values) {
         console.log(values)
+        const todo = {
+            id: id,
+            username: username,
+            description: values.description,
+            targetDate: values.targetDate,
+            done: false
+        }
+        console.log(todo)
+        updateTodoApi(username, id, todo)
+        .then(response => {
+            console.log(response)
+            navigate('/todos')
+        })
+        .catch(error => console.log(error))
     }
 
     function validate(values) {
